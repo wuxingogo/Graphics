577ebe50e906173f9f6604a4938a39c8f8de7163
Sebastien Lagarde <sebastien@unity3d.com>
2020-11-24 19:47:56 +0100

    Revert "Fixed an issue where a warning about the static sky not being ready was wrongly displayed. (#2727)"
    This reverts commit 4b1e36eb708101a60cb682b98925984dcd40a711.

--- com.unity.render-pipelines.high-definition/Runtime/Sky/SkyManager.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Sky/SkyManager.cs
@@ -153,12 +153,20 @@ namespace UnityEngine.Rendering.HighDefinition
 
         // 2 by default: Static sky + one dynamic. Will grow if needed.
         DynamicArray<CachedSkyContext> m_CachedSkyContexts = new DynamicArray<CachedSkyContext>(2);
-        
-        public SkyManager()	
-        {}	
 
-        ~SkyManager()	
-        {}        
+        public SkyManager()
+        {
+#if UNITY_EDITOR
+            UnityEditor.Lightmapping.bakeStarted += OnBakeStarted;
+    #endif
+        }
+
+        ~SkyManager()
+        {
+#if UNITY_EDITOR
+            UnityEditor.Lightmapping.bakeStarted -= OnBakeStarted;
+#endif
+        }
 
         internal static SkySettings GetSkySetting(VolumeStack stack)
         {
@@ -285,10 +293,6 @@ namespace UnityEngine.Rendering.HighDefinition
             }
 
             InitializeBlackCubemapArray();
-
-#if UNITY_EDITOR
-            UnityEditor.Lightmapping.bakeStarted += OnBakeStarted;
-#endif
         }
 
         void InitializeBlackCubemapArray()
@@ -334,7 +338,6 @@ namespace UnityEngine.Rendering.HighDefinition
 
 #if UNITY_EDITOR
             CoreUtils.Destroy(m_DefaultPreviewSky);
-            UnityEditor.Lightmapping.bakeStarted -= OnBakeStarted;
 #endif
         }
 
