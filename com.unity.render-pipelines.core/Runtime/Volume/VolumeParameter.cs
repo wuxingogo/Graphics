7c3c755fff914ff9fd06a59de3e0b6a0aa5e88ad
Sebastien Lagarde <sebastien@unity3d.com>
2020-11-24 19:49:40 +0100

    Revert "Allow cubemap volume parameter to accept RenderTextures [2021.1] (#2258)"
    This reverts commit 58e1b9d0fa130153f925173df08c3ce255367e35.

--- com.unity.render-pipelines.core/Runtime/Volume/VolumeParameter.cs
+++ com.unity.render-pipelines.core/Runtime/Volume/VolumeParameter.cs
@@ -1475,15 +1475,16 @@ namespace UnityEngine.Rendering
     /// A <see cref="VolumeParameter"/> that holds a <c>Cubemap</c> value.
     /// </summary>
     [Serializable, DebuggerDisplay(k_DebuggerDisplay)]
-    public class CubemapParameter : VolumeParameter<Texture>
+    public class CubemapParameter : VolumeParameter<Cubemap>
     {
         /// <summary>
         /// Creates a new <seealso cref="CubemapParameter"/> instance.
         /// </summary>
         /// <param name="value">The initial value to store in the parameter.</param>
         /// <param name="overrideState">The initial override state for the parameter.</param>
-        public CubemapParameter(Texture value, bool overrideState = false)
+        public CubemapParameter(Cubemap value, bool overrideState = false)
             : base(value, overrideState) {}
+
         // TODO: Cubemap interpolation
     }
 
