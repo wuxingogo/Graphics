efa4f02bc6223fb864a494a95969fd7bed88d67b
Sebastien Lagarde <sebastien@unity3d.com>
2020-11-27 10:59:09 +0100

    Revert "Performance/separate builds (#2672)"
    This reverts commit bdb28b3f3024a1492d15d71ab18824403ad23f1b.

--- .yamato/ruamel/jobs/projects/project_standalone_build.py
+++ .yamato/ruamel/jobs/projects/project_standalone_build.py
@@ -17,8 +17,5 @@ class Project_StandaloneBuildJob():
         project_folder = project.get("folder_standalone", project["folder"])
         cmd = get_cmd(platform["name"], api, 'standalone_build', "")
         job = _job(project, f'{test_platform["name"]}_build', editor, platform, api, cmd(project_folder, platform, api, test_platform, editor, build_config, color_space), build_config, color_space)
-        if project["name"] == "URP_Performance_BoatAttack":
-            job.add_artifacts_players_extra()
-        else:
-            job.add_artifacts_players()
+        job.add_artifacts_players()
         return job
