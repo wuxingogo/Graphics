683f6139e3271d11434aa34d2d7fabc365093338
Sebastien Lagarde <sebastien@unity3d.com>
2020-12-06 23:45:19 +0100

    Update HDRenderPipeline.LightLoop.cs

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.LightLoop.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.LightLoop.cs
@@ -602,7 +602,7 @@ namespace UnityEngine.Rendering.HighDefinition
                                                 ComputeBufferHandle bigTileLightList,
                                                 int                 frameIndex)
         {
-            if (Fog.IsVolumetricFogEnabled(hdCamera) && m_VisibleVolumeBounds.Count > 0)
+            if (Fog.IsVolumetricFogEnabled(hdCamera))
             {
                 using (var builder = renderGraph.AddRenderPass<VolumeVoxelizationPassData>("Volume Voxelization", out var passData))
                 {
